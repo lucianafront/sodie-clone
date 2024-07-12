53dacb7ad060df5675d4428a5ca5bc2c86b673c3
Luciana de Souza Felix <luciana.quimicatec@outlook.com>
2024-07-12 13:44:12 -0300

    Criado componente Botão.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# Clone :https://sodiedoces.com.br/bolos/
-## Criar pasta componente e dentro da pasta componentes criar arquivos (Cabecalhos.js ;Carrosel.js ;Sabores.js;Pedidos.js;vitrine.js;Sobre.js)
-## No App.Js importar todos os arquivos.
-### Criar fluxograma (salvo em pdf)
+# Clone :https://sodiedoces.com.br/bolos
+## Foi criado componentes Botão que pode ser reutilizado em todos os elementos.
--- src/componentes/Pedidos.js
+++ src/componentes/Pedidos.js
@@ -1,19 +1,36 @@
+import Botao from "./Botao";
+
 function Pedidos() {
     return (
      
 
       <div className="container">
       <div className="row">
-        <div className="col-md-2"></div>
+      
         <div className="col-md-8">
           <div className="center">
             <img src="https://sodiedoces.com.br/wp-content/themes/sodiedoces/img/app-rodape-icon.png" alt="Baixe nosso app"/>
             <h3>Um clique para adoçar seu dia! <strong>Baixe nosso app.</strong></h3>
-            <a href="https://play.google.com/store/apps/details?id=com.sodiedoces"><img src="https://sodiedoces.com.br/wp-content/themes/sodiedoces/img/google-play.png" alt="Google Play"/></a>
-            <a href="https://apps.apple.com/br/app/sodi%C3%AA-doces-oficial/id1557040152"><img src="https://sodiedoces.com.br/wp-content/themes/sodiedoces/img/app-store.png" alt="App Store"/></a>
+            <Botao
+            link='https://play.google.com/store/apps/details?id=com.sodiedoces'
+            ico=''
+
+            />
+         
+              <img src="https://sodiedoces.com.br/wp-content/themes/sodiedoces/img/google-play.png" alt="Google Play"/>
+
+            <Botao
+            link='https://apps.apple.com/br/app/sodi%C3%AA-doces-oficial/id1557040152'
+            ico=''
+            />
+        
+           
+         
+              
+              <img src="https://sodiedoces.com.br/wp-content/themes/sodiedoces/img/app-store.png" alt="App Store"/>
           </div>
-        </div>
-        <div className="col-md-2"></div>
+          </div>     
+      
       </div>
     </div>
   
